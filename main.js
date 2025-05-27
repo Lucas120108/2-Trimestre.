0de634e474d5bba391d5eaad1af8ae653cdb826e
Lucas120108 <161318654+Lucas120108@users.noreply.github.com>
2025-05-27 16:50:16 +0000

    .

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0) {
         return [dias, horas, minutos, segundos];
     } else {
-        return [3, 0, 0, 0];
+        return [254, 7, 59, 55];
     }
 }
 
